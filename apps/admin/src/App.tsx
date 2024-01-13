364001f5c5a8995ba022372602505d940f5e0a71
Tisha Murvihill <tmurv@shaw.ca>
2024-01-13 11:00:00 -0700

    initial commit

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import {Header} from "@varsityclinic/components";
+
+function App() {
+
+  return (
+    <>
+        <Header text="Is it sharing?" />
+    </>
+  )
+}
+
+export default App
